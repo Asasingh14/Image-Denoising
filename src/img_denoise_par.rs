14e035a447294d578afe4646a0befaac8ddc73bd
Asa <92502339+Singh1403@users.noreply.github.com>
2023-04-04 03:23:51 +0700

    Chunky Commit

---
+++ src/img_denoise_par.rs
@@ -0,0 +1,83 @@
+use std::fs::File;
+use image::{GenericImageView, ImageBuffer, Rgb};
+use std::path::Path;
+use image::codecs::jpeg::JpegEncoder;
+use rayon::prelude::*;
+
+pub fn denoise_image_par(input_path: &str, output_path: &str) {
+
+    // Open image
+    let image = image::open(&Path::new(input_path)).unwrap();
+    // Get dimensions of image
+    let (width, height) = image.dimensions();
+
+    // Create a new image buffer with the same dimensions as the input image
+    let mut denoised_image: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
+
+    // Use Rayon's parallel iterator to process each pixel in parallel
+    denoised_image
+        .enumerate_pixels_mut()
+        .par_bridge()
+        .for_each(|(x, y, pixel)| {
+            // Get neighbouring pixels for each pixel
+            let neighbors = get_neighbors(&image, x as i32, y as i32);
+            //Get average color of neighbouring pixels
+            let avg_color = average_color(&neighbors);
+            //Return a pixel with avg color
+            *pixel = Rgb([avg_color[0], avg_color[1], avg_color[2]]);
+        });
+
+    denoised_image.save(output_path).unwrap();
+}
+
+fn get_neighbors(image: &image::DynamicImage, x: i32, y: i32) -> Vec<[u8; 3]> {
+    let width = image.width() as i32;
+    let height = image.height() as i32;
+
+    /*
+    Get offsets for the diff pixels around the target pixels
+      -1 -1  |  0 -1  |  1 -1
+      -1  0  |  0  0  |  1  0
+      -1  1  |  0  1  |  1  1
+     */
+    let dx = [-1, 0, 1, -1, 1, -1, 0, 1];
+    let dy = [-1, -1, -1, 0, 0, 1, 1, 1];
+
+
+    //Process neighbouring pixel offsets in parallel
+    dx.par_iter()
+        .zip(dy.par_iter())
+        .filter_map(|(&dx, &dy)| {
+            let nx = x + dx;
+            let ny = y + dy;
+
+            if nx >= 0 && ny >= 0 && nx < width && ny < height {
+                let pixel = image.get_pixel(nx as u32, ny as u32);
+                Some([pixel[0], pixel[1], pixel[2]])
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<[u8; 3]>>()
+}
+
+fn average_color(neighbors: &Vec<[u8; 3]>) -> [u8; 3] {
+    let count = neighbors.len() as u32;
+    let mut r_sum = 0;
+    let mut g_sum = 0;
+    let mut b_sum = 0;
+
+    //Sum the red, green, and blue components of all colors
+    for color in neighbors {
+        r_sum += color[0] as u32;
+        g_sum += color[1] as u32;
+        b_sum += color[2] as u32;
+    }
+
+    // Calculate the average of red, green, and blue components and return the result as a color
+    [
+        (r_sum / count) as u8,
+        (g_sum / count) as u8,
+        (b_sum / count) as u8,
+    ]
+}
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::fs;
+use std::io::Result;
+use std::path::Path;
+use std::time::Instant;
+
+use crate::img_denoise::*;
+use crate::img_denoise_imp::*;
+use crate::img_denoise_par::*;
+
+mod img_denoise;
+mod img_denoise_imp;
+mod img_denoise_par;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut start = Instant::now();
+    test_img_denoise(); //Basic Denoise
+    println!("Basic Denoise time: {:?}", Instant::now() - start);
+
+    let mut start = Instant::now();
+    test_img_denoise_par();//Basic Denoise Parallel
+    println!("Basic Denoise parallel time: {:?}", Instant::now() - start);
+
+    let mut start = Instant::now();
+    test_img_denoise_imp(); //Gaussian Denoise
+    println!("Gaussian Denoise time: {:?}", Instant::now() - start);
+
+}
+
+fn test_img_denoise() {
+    let input_file_1 = "test_file/image1.jpeg";
+    let output_file_1 = "res_dump/out1.jpeg";
+    denoise_image(input_file_1, output_file_1);
+
+    let input_file_2 = "test_file/image2.jpg";
+    let output_file_2 = "res_dump/out2.jpeg";
+    denoise_image(input_file_2, output_file_2);
+
+    let input_file_3 = "test_file/image3.jpg";
+    let output_file_3 = "res_dump/out3.jpeg";
+    denoise_image(input_file_3, output_file_3);
+
+
+    let input_file_4 = "test_file/image4.jpg";
+    let output_file_4 = "res_dump/out4.jpeg";
+    denoise_image(input_file_4, output_file_4);
+
+    let input_file_4 = "test_file/image5.jpg";
+    let output_file_4 = "res_dump/out5.jpeg";
+    denoise_image(input_file_4, output_file_4);
+
+    let input_file_4 = "test_file/image6.jpg";
+    let output_file_4 = "res_dump/out6.jpeg";
+    denoise_image(input_file_4, output_file_4);
+}
+
+fn test_img_denoise_par() {
+    let input_file_1 = "test_file/image1.jpeg";
+    let output_file_1 = "res_dump/out1_par.jpeg";
+    denoise_image_par(input_file_1, output_file_1);
+
+    let input_file_2 = "test_file/image2.jpg";
+    let output_file_2 = "res_dump/out2_par.jpeg";
+    denoise_image_par(input_file_2, output_file_2);
+
+    let input_file_3 = "test_file/image3.jpg";
+    let output_file_3 = "res_dump/out3_par.jpeg";
+    denoise_image_par(input_file_3, output_file_3);
+
+
+    let input_file_4 = "test_file/image4.jpg";
+    let output_file_4 = "res_dump/out4_par.jpeg";
+    denoise_image_par(input_file_4, output_file_4);
+
+    let input_file_4 = "test_file/image5.jpg";
+    let output_file_4 = "res_dump/out5_par.jpeg";
+    denoise_image_par(input_file_4, output_file_4);
+
+    let input_file_4 = "test_file/image6.jpg";
+    let output_file_4 = "res_dump/out6_par.jpeg";
+    denoise_image_par(input_file_4, output_file_4);
+}
+
+fn test_img_denoise_imp() {
+    let input_file_1 = "test_file/image1.jpeg";
+    let output_file_1 = "res_dump/out1_imp.jpeg";
+    denoise_image_imp(input_file_1, output_file_1, 1.5, 1.0);
+
+    let input_file_2 = "test_file/image2.jpg";
+    let output_file_2 = "res_dump/out2_imp.jpeg";
+    denoise_image_imp(input_file_2, output_file_2, 1.5, 1.0);
+
+    let input_file_3 = "test_file/image3.jpg";
+    let output_file_3 = "res_dump/out3_imp.jpeg";
+    denoise_image_imp(input_file_3, output_file_3, 1.5, 1.0);
+
+    let input_file_4 = "test_file/image4.jpg";
+    let output_file_4 = "res_dump/out4_imp.jpeg";
+    denoise_image_imp(input_file_4, output_file_4, 1.5, 1.0);
+
+    let input_file_5 = "test_file/image5.jpg";
+    let output_file_5 = "res_dump/out5_imp.jpeg";
+    denoise_image_imp(input_file_5, output_file_5, 1.5, 1.0);
+
+    let input_file_6 = "test_file/image6.jpg";
+    let output_file_6 = "res_dump/out6_imp.jpeg";
+    denoise_image_imp(input_file_6, output_file_6, 1.5, 1.0);
+
+}
